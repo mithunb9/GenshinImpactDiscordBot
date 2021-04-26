ad085c91e31446ca8eed2a9b0a2eca3d6d968c57
sendmithunb@gmail.com <sendmithunb@gmail.com>
2021-04-26 12:26:27 -0500

    add artifacts

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+const { getArtifactData } = require("./stats/stats.js");
+
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
@@ -9,10 +11,18 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("message", (msg) => {
+client.on("message", async (msg) => {
   if (msg.content === "mithun") {
     msg.reply("Siddarth is gay");
-  } else if (msg.content === "stats") {
+  } else if (msg.content === "artifacts") {
+    const data = await getArtifactData();
+    console.log(data);
+
+    const artifactEmbed = new Discord.MessageEmbed()
+      .setTitle("Artifacts")
+      .setDescription(data);
+
+    msg.reply(artifactEmbed);
   }
 });
 
--- stats/stats.js
+++ stats/stats.js
@@ -1,5 +1,70 @@
 const axios = require("axios").default;
 
-axios.get("https://api.genshin.dev/artifacts").then((res) => {
-  console.log(res.data);
-});
+const getArtifactData = async (scope = "") => {
+  const response = await axios.get(
+    `https://api.genshin.dev/artifacts/${scope}`
+  );
+
+  return response.data;
+};
+
+let oldData = [
+  "adventurer",
+  "archaic-petra",
+  "berserker",
+  "blizzard-strayer",
+  "bloodstained-chivalry",
+  "brave-heart",
+  "crimson-witch-of-flames",
+  "defender-s-will",
+  "gambler",
+  "gladiator-s-finale",
+  "heart-of-depth",
+  "instructor",
+  "lavawalker",
+  "lucky-dog",
+  "maiden-beloved",
+  "martial-artist",
+  "noblesse-oblige",
+  "resolution-of-sojourner",
+  "retracing-bolide",
+  "scholar",
+  "the-exile",
+  "thundering-fury",
+  "thundersoother",
+  "tiny-miracle",
+  "traveling-doctor",
+  "viridescent-venerer",
+  "wanderer-s-troupe",
+];
+let capitalizedData = [
+  "Adventurer",
+  "Archaic-petra",
+  "Berserker",
+  "Blizzard-strayer",
+  "Bloodstained-chivalry",
+  "Brave-heart",
+  "Crimson-witch-of-flames",
+  "Defender-s-will",
+  "Gambler",
+  "Gladiator-s-finale",
+  "Heart-of-depth",
+  "Instructor",
+  "Lavawalker",
+  "Lucky-dog",
+  "Maiden-beloved",
+  "Martial-artist",
+  "Noblesse-oblige",
+  "Resolution-of-sojourner",
+  "Retracing-bolide",
+  "Scholar",
+  "The-exile",
+  "Thundering-fury",
+  "Thundersoother",
+  "Tiny-miracle",
+  "Traveling-doctor",
+  "Viridescent-venerer",
+  "Wanderer-s-troupe",
+];
+
+module.exports = { getArtifactData };
